580afcee4b96189550208338830d00d3f01bad22
Zakariya Ahmed <zahmed@credera.com>
2017-09-23 16:46:21 -0500

    refactoring application

--- src/main/java/com/zakahmed/app/GundamModelKitApplication.java
+++ src/main/java/com/zakahmed/app/GundamModelKitApplication.java
@@ -7,9 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Created by zahmed on 9/23/17.
  */
 @SpringBootApplication
-public class Application {
+public class GundamModelKitApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(GundamModelKitApplication.class, args);
     }
 }
