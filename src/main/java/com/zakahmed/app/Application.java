fd9bb52f4b7754fa467df67a25c096af5f38673a
Zakariya Ahmed <zahmed@credera.com>
2017-09-23 16:25:10 -0500

    SpringBootApplication boilerplate

---
+++ src/main/java/com/zakahmed/app/Application.java
@@ -0,0 +1,15 @@
+package com.zakahmed.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zahmed on 9/23/17.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
