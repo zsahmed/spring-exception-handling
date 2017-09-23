6a580e3da25a56c0b4aa8bd04fbbbd0a796dc3c8
Zakariya Ahmed <zahmed@credera.com>
2017-09-23 16:25:25 -0500

    controller file

---
+++ src/main/java/com/zakahmed/controller/Resource.java
@@ -0,0 +1,12 @@
+package com.zakahmed.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zahmed on 9/23/17.
+ */
+@RestController
+public class Resource {
+
+
+}
