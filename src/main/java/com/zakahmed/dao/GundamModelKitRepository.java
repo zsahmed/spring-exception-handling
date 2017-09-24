2769c68a051354b61abe8b344c1fa01478e46da6
Zakariya Ahmed <zahmed@credera.com>
2017-09-24 17:42:49 -0500

    dependencies

--- src/main/java/com/zakahmed/dao/GundamModelKitRepository.java
+++ src/main/java/com/zakahmed/dao/GundamModelKitRepository.java
@@ -1,7 +1,11 @@
 package com.zakahmed.dao;
 
+import com.zakahmed.domain.GundamModelKit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 /**
  * Created by zahmed on 9/24/17.
  */
-public interface GundamModelKitRepository  {
+@Repository
+public interface GundamModelKitRepository extends JpaRepository<GundamModelKit, Long> {
 }
