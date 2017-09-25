24753da80ba648c57e711672f943f73f8e349459
Zakariya Ahmed <zahmed@credera.com>
2017-09-25 12:29:30 -0500

    using @Entity, @Id, and @Generated on GundamModelKit domain object to be used to create relational table for this object

--- src/main/java/com/zakahmed/domain/GundamModelKit.java
+++ src/main/java/com/zakahmed/domain/GundamModelKit.java
@@ -4,14 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.*;
+
 /**
  * Created by zahmed on 9/23/17.
  */
 
 @Getter @Setter
 @AllArgsConstructor @NoArgsConstructor
+@Entity
 public class GundamModelKit {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String imageUrl;
     private String link;
