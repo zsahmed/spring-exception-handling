6fbebd70d75e73d43ff2ca3b512f0ef18936888f
Zakariya Ahmed <zahmed@credera.com>
2017-09-25 12:28:56 -0500

    added POST method to add items to db

--- src/main/java/com/zakahmed/controller/Resource.java
+++ src/main/java/com/zakahmed/controller/Resource.java
@@ -1,7 +1,9 @@
 package com.zakahmed.controller;
 
 import com.zakahmed.domain.GundamModelKit;
+import com.zakahmed.service.GundamModelKitService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +20,29 @@ import java.util.List;
 public class Resource {
     public final static String API_URL = "/api/model-kit";
 
+    @Autowired
+    private GundamModelKitService gundamModelKitService;
+
     @RequestMapping(value="", method = RequestMethod.GET)
     public List<GundamModelKit> getModelKits() {
         log.info("getModelKits called");
-        List modelKitList = new ArrayList();
-        modelKitList.add("Hello There");
+        List modelKitList = gundamModelKitService.getModelKits();
+
         return  modelKitList;
+    }
+
+    @RequestMapping(value="/add", method = RequestMethod.POST)
+    public boolean addGunplaKit() {
+        GundamModelKit model = new GundamModelKit();
+        model.setTitle("Zaku");
+        model.setImageUrl("image");
+        model.setLink("link");
+        model.setPrice("10");
+        model.setRelease_date("October");
+        model.setSeries("Mobile Suit Gundam");
+
+        gundamModelKitService.save(model);
+        return true;
     }
 
 }
