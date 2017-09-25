4f4e3a99d6dcedcd3f1974bde28ce9ccb34d9505
Zakariya Ahmed <zahmed@credera.com>
2017-09-25 10:49:20 -0500

    mocked out GET request to resource. Can now hit endpoint via postman

--- src/main/java/com/zakahmed/controller/Resource.java
+++ src/main/java/com/zakahmed/controller/Resource.java
@@ -1,12 +1,27 @@
-package com.zakahmed.controller;
+package com.zakahmed.app;
 
+import com.zakahmed.domain.GundamModelKit;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by zahmed on 9/23/17.
  */
 @RestController
+@Slf4j
 public class Resource {
+    public final static String API_URL = "/api/model-kit";
 
+    @RequestMapping(value="/greeting")
+    public List<GundamModelKit> getModelKits() {
+        log.info("getModelKits called");
+        List modelKitList = new ArrayList();
+        modelKitList.add("Hello There");
+        return  modelKitList;
+    }
 
 }
